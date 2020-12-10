35d8926622c260cad68de730bb4acaf2b9f2fbac
KendalH98 <kendal.hall@bison.howard.edu>
2020-12-09 19:51:04 -0500

    Changed the configuration of certain files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './img/logo.png';
 import './App.css';
+import Game from './components/Game';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="header">
+          {/* TODO: Add Boggle Image */}
+          <img src={logo} className="header-logo" alt="logo" />
+        </div>
+        {/* TODO: Render Game component */}
+        <Game />
+      </div>
+    );
+  }
 }
 
 export default App;
+
